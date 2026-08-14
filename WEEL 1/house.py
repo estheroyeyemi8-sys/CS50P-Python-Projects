2e699bc150321927f62e2ad45aee0b83e8d4c32f
estheroyeyemi8-sys <estheroyeyemi8@gmail.com>
2026-08-14 04:29:04 +0100

    Delete WEEL 1/house.py

--- WEEL 1/house.py
+++
@@ -1,9 +0,0 @@
-name = input("What's your name? ")
-
-match name:
-    case "Harry" | "Hermione" | "Ron":
-        print("Duplex")
-    case "Oluwaseyi":
-        print("Mansion")
-    case _: 
-        print("Who?")
